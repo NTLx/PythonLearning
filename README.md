0eea926816830df6eabad64e0d22e23a1173ccd4
NTLx <lx@Macmini.local>
2020-04-05 21:31:32 +0800

    1st day learning

---
+++ README.md
@@ -0,0 +1,3 @@
+Learn Python uisng [Learn Python The Hard Way](https://learnpythonthehardway.org/python3) as my start.
+
+> I'm using the 4th edition of `Learn Python The Hard Way`, it's now using Python 3 by default.
---
+++ ex3.py
@@ -0,0 +1,26 @@
+# What is the order of operations?
+# In the United States we use an acronym called PEMDAS which stands for Parentheses Exponents Multiplication Division Addition Subtraction. That's the order Python follows as well. The mistake people make with PEMDAS is to think this is a strict order, as in "Do P, then E, then M, then D, then A, then S." The actual order is you do the multiplication and division (M&D) in one step, from left to right, then you do the addition and subtraction in one step from left to right. So, you could rewrite PEMDAS as PE(M&D)(A&S).
+
+print("I will now count my chickens:")
+
+print ("Hens", 25+30/6)
+print ("Roosters",100-25*3%4)
+
+print ("Now I will count the eggs:")
+
+print(3+2+1-5+4%2-1/4+6)
+
+print("Is it true that 3+2<5-7 ?")
+
+print(3+2<5-7)
+
+print("What is 3+2 ?",3+2)
+print("What is 5-7 ?",5-7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater ?",5>-2)
+print("Is it greater or equal ?",5>=-2)
+print("Is it less or equal ?",5<=-2)
